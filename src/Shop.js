e9beb6a9fa66c3237601cc5e8e81885c56055ae1
abhinav-dholi <81688737+abhinav-dholi@users.noreply.github.com>
2024-01-30 13:04:14 -0500

    redux base app

---
+++ src/Shop.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Shop = () => {
+  return (
+    <div>
+        <h2>Buy xyz for Rs. 50</h2>
+        <button className="btn btn-primary max-2">-</button>
+        Add this item to cart
+        <button className="btn btn-primary max-2">+</button>
+    </div>
+  )
+}
+
+export default Shop
